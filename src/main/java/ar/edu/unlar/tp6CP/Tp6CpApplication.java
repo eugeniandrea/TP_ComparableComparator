aba2824fbb34ae864b5728d8660c62bfc9091436
eugeniandrea <andrxgi@gmail.com>
2026-06-18 02:36:00 -0300

    Fin del TP: subo el pom y la configuracion de arranque de la aplicacion

---
+++ src/main/java/ar/edu/unlar/tp6CP/Tp6CpApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.tp6CP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6CpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp6CpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unlar/tp6CP/Tp6CpApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlar.tp6CP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6CpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
